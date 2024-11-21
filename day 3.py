77e397e4c4bb456c358657537fbf9214490efbf0
MahendraN <mahendraramalingam8056@gmail.com>
2024-11-21 06:12:01 +0530

    Update

--- day 3.py
+++ day 3.py
@@ -214,9 +214,6 @@ NOTE: Give details and register the form
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Firefox()
