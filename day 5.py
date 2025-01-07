9e72fe9a1a6805d75bf9db974c5d2680c3f32fde
MahendraN <mahendraramalingam8056@gmail.com>
2025-01-07 21:11:31 +0530

    Update

--- day 5.py
+++ day 5.py
@@ -231,3 +231,231 @@ except Exception as e:
 # Close the WebDriver
 driver.quit()
 xxxxxxxxxxxxxxxxxxxx"""
+
+"""
+QUESTION 4
+----------
+URL : https://www.shopclues.com/wholesale.html
+ 
+NOTE: Mobile and electronics is first mouseover 
+      Click Smart Phones range Rs5001 - Rs10000
+
+"""
+"""
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+# Initialize the driver
+driver = webdriver.Chrome()
+
+# Open the ShopClues website
+url = "https://www.shopclues.com/wholesale.html"
+driver.get(url)
+
+# Maximize the window
+driver.maximize_window()
+
+# Add a short delay to allow the page to load
+time.sleep(3)
+
+# Locate the "Mobile and Electronics" menu
+mobile_and_electronics = driver.find_element("xpath", "//a[contains(text(), 'Mobiles & Electronics')]")
+
+# Perform mouseover action
+action = ActionChains(driver)
+action.move_to_element(mobile_and_electronics).perform()
+
+# Add a short delay to ensure the submenu loads
+time.sleep(2)
+
+# Locate and click the "Smart Phones Range Rs5001 - Rs10000" link
+smartphones_range = driver.find_element("xpath", "//a[contains(text(), 'Smartphones Range Rs5001 - Rs10000')]")
+smartphones_range.click()
+
+# Add a short delay to observe the result
+time.sleep(5)
+
+# Close the browser
+driver.quit()
+
+xxxxxxxxxxxxxxxxxxxxxxx"""
+
+"""
+QUESTION 5
+----------
+URL : https://www.shopclues.com/wholesale.html 
+ 
+NOTE: Sports&more  is first mouseover
+      Click weights grainers 
+
+"""
+"""
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+# Initialize the driver
+driver = webdriver.Chrome()
+
+# Open the ShopClues website
+url = "https://www.shopclues.com/wholesale.html"
+driver.get(url)
+
+# Maximize the browser window
+driver.maximize_window()
+
+# Add a short delay to allow the page to load completely
+time.sleep(3)
+
+# Locate the "Sports & More" menu
+sports_and_more = driver.find_element("xpath", "//a[contains(text(), 'Sports & More')]")
+
+# Perform mouseover action
+action = ActionChains(driver)
+action.move_to_element(sports_and_more).perform()
+
+# Add a short delay to ensure the submenu appears
+time.sleep(2)
+
+# Locate and click the "Weights Grainers" link
+weights_trainers = driver.find_element("xpath", "//a[contains(text(), 'Weights Grainers')]")
+weights_trainers.click()
+
+# Add a short delay to observe the result
+time.sleep(5)
+
+# Close the browser
+driver.quit()
+
+xxxxxxxxxxxxxxxxxxxxxxxxx"""
+
+"""
+QUESTION 6 
+----------
+URL : http://greenstech.in/selenium-course-content.html
+
+NOTE: Course is first mouseover
+      Software testing training is second mouseover  
+      Click selenium training 
+
+"""
+"""
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+# Initialize the WebDriver
+driver = webdriver.Chrome()
+
+# Open the specified URL
+url = "http://greenstech.in/selenium-course-content.html"
+driver.get(url)
+
+# Maximize the browser window
+driver.maximize_window()
+
+# Add a short delay to allow the page to load completely
+time.sleep(3)
+
+# Locate the "Course" menu
+course_menu = driver.find_element("xpath", "//div[contains(text(), 'Course')]")
+
+# Perform mouseover on the "Course" menu
+action = ActionChains(driver)
+action.move_to_element(course_menu).perform()
+
+# Add a short delay to ensure the submenu loads
+time.sleep(2)
+
+# Locate the "Software Testing Training" submenu
+software_testing_training = driver.find_element("xpath", "//span[text()='Software Testing Training']")
+
+# Perform mouseover on the "Software Testing Training" submenu
+action.move_to_element(software_testing_training).perform()
+
+# Add a short delay to ensure the next submenu loads
+time.sleep(2)
+
+# Locate and click the "Selenium Training" link
+selenium_training = driver.find_element("xpath", "//span[text()='Selenium Training']")
+selenium_training.click()
+
+# Add a short delay to observe the result
+time.sleep(5)
+
+# Close the browser
+driver.quit()
+
+xxxxxxxxxxxxxxxxxxxxxxxx"""
+
+"""
+QUESTION 7
+-----------
+URL : http://greenstech.in/selenium-course-content.html
+
+NOTE: Courses is first mouseover
+      Oracle training is second mouseover
+      Click Oracle sql training
+
+"""
+"""
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+# Initialize the WebDriver
+driver = webdriver.Chrome()
+
+# Open the specified URL
+url = "http://greenstech.in/selenium-course-content.html"
+driver.get(url)
+
+# Maximize the browser window
+driver.maximize_window()
+
+# Add a short delay to allow the page to load completely
+time.sleep(3)
+
+# Locate the "Courses" menu
+courses_menu = driver.find_element("xpath", "//div[contains(text(), 'Courses')]")
+
+# Perform mouseover on the "Courses" menu
+action = ActionChains(driver)
+action.move_to_element(courses_menu).perform()
+
+# Add a short delay to ensure the submenu loads
+time.sleep(2)
+
+# Locate the "Oracle Training" submenu
+oracle_training = driver.find_element("xpath", "//span[text()='Oracle Training']")
+
+# Perform mouseover on the "Oracle Training" submenu
+action.move_to_element(oracle_training).perform()
+
+# Add a short delay to ensure the next submenu loads
+time.sleep(2)
+
+# Locate and click the "Oracle SQL Training" link
+oracle_sql_training = driver.find_element("xpath", "//span[text()='Oracle SQL Training']")
+oracle_sql_training.click()
+
+# Add a short delay to observe the result
+time.sleep(5)
+
+# Close the browser
+driver.quit()
+
+xxxxxxxxxxxxxxxxxxxxxxxxxx"""
+
+"""
+
+QUESTION 8
+----------
+URL : http://greenstech.in/selenium-course-content.html
+
+NOTE: Courses  is first mouseover
+      DataWarehouse training is second mouse over
+      click microstategy training.
+
+"""
